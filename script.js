3d22f6060c185284285236566f37483d983362c4
hentschellj <hentschellj@gmail.com>
2020-02-04 18:55:54 -0600

    Created new variable for main body

--- script.js
+++ script.js
@@ -1,22 +1,23 @@
 $(document).ready(function() {
   let $barMain = $('.topBar-main');
   let $barLever = $('.topBar-lever');
+  let $body = $('body');
   let $bodyMargin = parseInt($('body').css('margin-top').replace('px', ''));
   let $barHeight = $('.topBar-main').outerHeight();
 
   $barMain.show();
-  $('body').css('margin', ($bodyMargin + $barHeight));
+  $body.css('margin', ($bodyMargin + $barHeight));
   $barLever.hide();
 
   $('.topBar-close').on('click', function() {
     $barMain.slideToggle('200');
     $barLever.delay('400').slideToggle('200');
-    $('body').css('margin', $bodyMargin);
+    $body.css('margin', $bodyMargin);
   });
 
   $barLever.on('click', function() {
     $barMain.delay('400').slideToggle('200');
     $(this).slideToggle('200');
-    $('body').css('margin', ($bodyMargin + $barHeight));
+    $body.css('margin', ($bodyMargin + $barHeight));
   });
 });
