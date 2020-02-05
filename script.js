e7edb0c0ec4e6dcfd48946b3a746fd2737ab3b23
hentschellj <hentschellj@gmail.com>
2020-02-04 18:51:06 -0600

    Created variables for the body margins and top height

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 $(document).ready(function() {
   let $barMain = $('.topBar-main');
   let $barLever = $('.topBar-lever');
+  let $bodyMargin = parseInt($('body').css('margin-top').replace('px', ''));
+  let $barHeight = $('.topBar-main').outerHeight();
+  console.log($bodyMargin);
+  console.log($barHeight);
 
   $barMain.show();
   $barLever.hide();
